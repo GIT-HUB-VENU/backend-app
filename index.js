601c5a8227d3ae21d28d125fffa995c00802304e
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-09 15:03:17 +0530

    updated

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
 import dotenv from "dotenv";
 
-// import {productRouter} from "./routes/productRoute.js";
 import { storeRouter } from "./routes/storeRoute.js";
-import { connect } from "mongoose";
 import connectDB from "./config/db.js";
+
 const app = express();
 
 dotenv.config();
@@ -14,6 +13,7 @@ dotenv.config();
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -22,24 +22,19 @@ app.use(
     secret: "secretkey",
     resave: false,
     saveUninitialized: false,
-  }),
+  })
 );
 
 app.use("/", storeRouter);
-// app.use("/auth", authRouter);
-// app.use("/products", productRouter);
-// app.use("/users", userRouter);
+
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDB();
-  app.listen(5000, () => {
-    console.log("Server Started");
+
+  app.listen(PORT, () => {
+    console.log("Server Started on port " + PORT);
   });
 };
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Server Started on port " + PORT);
-});
 
 startServer();
