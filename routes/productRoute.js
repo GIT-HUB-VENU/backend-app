0a4b5a44d1d284dc43a19e4c8d4c70a0c29ba9b1
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-11 09:54:51 +0530

    updated

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,12 +1,19 @@
-import express from "express"
-import { getProducts , addProductForm , addProduct , deleteProduct ,editProductForm, saveProduct } from "../controllers/productController.js";
-const productRouter = express.Router()
+import {
+  getProducts,
+  addProductForm,
+  addProduct,
+  deleteProduct,
+  editProductForm,
+  saveProduct,
+} from "../controllers/productController.js";
+import express from "express";
+const productRouter = express.Router();
 
-productRouter.get("/",getProducts)
-productRouter.get("/add",addProductForm)
-productRouter.post("/add",addProduct)
-productRouter.get("/:id/delete",deleteProduct)
-productRouter.get("/:id/edit",editProductForm)
-productRouter.post("/:id/save",saveProduct)
+productRouter.get("/", getProducts);
+productRouter.get("/add", addProductForm);
+productRouter.post("/add", addProduct);
+productRouter.get("/:id/delete", deleteProduct);
+productRouter.get("/:id/edit", editProductForm);
+productRouter.post("/:id/save", saveProduct);
 
-export {productRouter};
+export default productRouter;
