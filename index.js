60b3afa698540f0d1263db12303c3fa543db1f17
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-12 14:55:24 +0530

    updated

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req,res,next)=>{
+  console.log("REQUEST:", req.method, req.url);
+  next();
+});
+
 app.use("/auth", authRouter);
 app.use("/store", storeRouter);
 
