cfe011bd3f89239095330af8daa88546b536c870
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-12 14:19:54 +0530

    commit

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,13 @@
-import {login,validateUser,register,registerUser,logout,signup,signin} from "../controllers/authController.js"
-import express from "express"
-const authRouter = express.Router()
+import { login, validateUser, register, registerUser, logout, signup } from "../controllers/authController.js";
+import express from "express";
 
-authRouter.get("/login",login)
-authRouter.post("/login",validateUser)
-authRouter.get("/register",register)
-authRouter.post("/register",registerUser)
-authRouter.get("/logout",logout)
-authRouter.post("/signup",signup)
-authRouter.post("/signin",signin)
+const authRouter = express.Router();
 
-export default authRouter
+authRouter.get("/login", login);
+authRouter.post("/login", validateUser);
+authRouter.get("/register", register);
+authRouter.post("/register", registerUser);
+authRouter.get("/logout", logout);
+authRouter.post("/signup", signup);
+
+export default authRouter;
